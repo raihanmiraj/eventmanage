c9d9b26f4fd78e2430259bd00ced079658a524cd
Raihanmiraj <65871430+Raihanmiraj@users.noreply.github.com>
2022-01-04 22:14:41 +0600

    ok

---
+++ script.js
@@ -0,0 +1,23 @@
+const AddAnEvent = () =>{
+    var title = document.querySelector("#eventtitle").value;
+  
+    var alreadyExist =  localStorage.getItem("eventtitle")?JSON.parse(localStorage.getItem("eventtitle")):"";
+    if(Object.keys(alreadyExist).indexOf(title) == -1){
+        var addNewItem = {
+            ...alreadyExist,
+            [title]:""
+        }
+     localStorage.setItem("eventtitle",  JSON.stringify(addNewItem));
+   
+     if(!document.getElementById("alreadyexistevent").classList.contains("hide") ){
+        document.getElementById("alreadyexistevent").classList.add("hide")  ;
+    }
+    $("#eventtable").append('<tr> <td>'+title+'</td>  <td><a href=/ok.html?id='+title+'>view</a></td></tr>');
+    }else{
+        if(document.getElementById("alreadyexistevent").classList.contains("hide") ){
+            document.getElementById("alreadyexistevent").classList.remove("hide")  
+        }
+       
+    }
+
+}
