ca926265ae28b352ab541bc9f2b88853c5b756e4
Raihanmiraj <65871430+Raihanmiraj@users.noreply.github.com>
2022-01-05 13:01:49 +0600

    ok

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const config = {
 
 firebase.initializeApp(config);
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(swRegistered => {
@@ -26,105 +26,105 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-messaging
-  .requestPermission()
-  .then(() => {
-    return messaging.getToken();
-  })
-  .then(token => {
-    tokenString.innerHTML = "Token Is : " + token;
-    subscribeTokenToTopic(token, "allUsers");
-  })
-  .catch(err => {
-    console.log("Unable to get permission to notify", err);
-  });
+// messaging
+//   .requestPermission()
+//   .then(() => {
+//     return messaging.getToken();
+//   })
+//   .then(token => {
+//     tokenString.innerHTML = "Token Is : " + token;
+//     subscribeTokenToTopic(token, "allUsers");
+//   })
+//   .catch(err => {
+//     console.log("Unable to get permission to notify", err);
+//   });
 
-messaging.onMessage(payload => {
-  console.log("Message received. ", payload);
-  const { title, ...options } = payload.notification;
-});
+// messaging.onMessage(payload => {
+//   console.log("Message received. ", payload);
+//   const { title, ...options } = payload.notification;
+// });
 
-function toggleMenu() {
-  const navbar = document.getElementById("navbar");
-  if (navbar.className === "navbar") {
-    navbar.className += " responsive";
-  } else {
-    navbar.className = "navbar";
-  }
-}
+// function toggleMenu() {
+//   const navbar = document.getElementById("navbar");
+//   if (navbar.className === "navbar") {
+//     navbar.className += " responsive";
+//   } else {
+//     navbar.className = "navbar";
+//   }
+// }
 
-function notification() {
-  const options = {
-    body: "Testing Our Notification",
-    icon: "/icons/icon.png"
-  };
+// function notification() {
+//   const options = {
+//     body: "Keep Using Event Manager",
+//     icon: "/icons/icon.png"
+//   };
 
-  new Notification("Hi there!!", options);
-}
+//   new Notification("Hi there!!", options);
+// }
 
-function displayNotification() {
-  if (window.Notification && Notification.permission === "granted") {
-    notification();
-  } else if (window.Notification && Notification.permission !== "denied") {
-    Notification.requestPermission(status => {
-      if (status === "granted") {
-        notification();
-      } else {
-        alert("You denied or dismissed permissions to notifications.");
-      }
-    });
-  } else {
-    alert(
-      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
-    );
-  }
-}
+// function displayNotification() {
+//   if (window.Notification && Notification.permission === "granted") {
+//     notification();
+//   } else if (window.Notification && Notification.permission !== "denied") {
+//     Notification.requestPermission(status => {
+//       if (status === "granted") {
+//         notification();
+//       } else {
+//         alert("You denied or dismissed permissions to notifications.");
+//       }
+//     });
+//   } else {
+//     alert(
+//       "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
+//     );
+//   }
+// }
 
-function subscribeTokenToTopic(token, topic) {
-  console.log(token);
-  fetch("https://iid.googleapis.com/iid/v1/" + token + "/rel/topics/" + topic, {
-    method: "POST",
-    headers: new Headers({
-      Authorization:
-        "key=AAAAqdLGrJ8:APA91bENDji0OkGUIrM0VyMxhcNi_QdzqoS2bCPX1sWJiBjFtMAyEUa6iSb96xuhGeNWhWKgxCoczSdlW4S0PHjD9ybEWvhDP_wvBHOfgX89esOJwTD-q-0e31TagXRa4bKKb7OrXG4K"
-    })
-  })
-    .then(response => {
-      if (response.status < 200 || response.status >= 400) {
-        throw "Error subscribing to  the following topic: " +
-        response.status +
-        " - " +
-        response.text();
-      } else {
-        console.log('Successfully subscribed to "' + topic + '"');
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+// function subscribeTokenToTopic(token, topic) {
+//   console.log(token);
+//   fetch("https://iid.googleapis.com/iid/v1/" + token + "/rel/topics/" + topic, {
+//     method: "POST",
+//     headers: new Headers({
+//       Authorization:
+//         "key=AAAAqdLGrJ8:APA91bENDji0OkGUIrM0VyMxhcNi_QdzqoS2bCPX1sWJiBjFtMAyEUa6iSb96xuhGeNWhWKgxCoczSdlW4S0PHjD9ybEWvhDP_wvBHOfgX89esOJwTD-q-0e31TagXRa4bKKb7OrXG4K"
+//     })
+//   })
+//     .then(response => {
+//       if (response.status < 200 || response.status >= 400) {
+//         throw "Error subscribing to  the following topic: " +
+//         response.status +
+//         " - " +
+//         response.text();
+//       } else {
+//         console.log('Successfully subscribed to "' + topic + '"');
+//       }
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// }
 
-function onShare() {
-  const title = document.title;
-  const url = document.querySelector("link[rel=canonical]")
-    ? document.querySelector("link[rel=canonical]").href
-    : document.location.href;
-  const text = "Learn how to use the share api";
+// function onShare() {
+//   const title = document.title;
+//   const url = document.querySelector("link[rel=canonical]")
+//     ? document.querySelector("link[rel=canonical]").href
+//     : document.location.href;
+//   const text = "Learn how to use the share api";
 
-  if (navigator.share) {
-    navigator
-      .share({
-        title,
-        url,
-        text
-      })
-      .then(() => {
-        alert(`Thanks for Sharing!`);
-      })
-      .catch(err => {
-        alert(`Couldn't share ${err}`);
-      });
-  } else {
-    alert(`Not supported !!`);
-  }
-}
+//   if (navigator.share) {
+//     navigator
+//       .share({
+//         title,
+//         url,
+//         text
+//       })
+//       .then(() => {
+//         alert(`Thanks for Sharing!`);
+//       })
+//       .catch(err => {
+//         alert(`Couldn't share ${err}`);
+//       });
+//   } else {
+//     alert(`Not supported !!`);
+//   }
+// }
